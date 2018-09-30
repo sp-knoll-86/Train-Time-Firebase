6d9ba59be758de81afa71a8b2508b4ddf8620a5b
DrF <sp.knoll.86@gmail.com>
2018-09-29 21:40:30 -0500

    Train-Time-Firebase

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Train-Time-Firebase
+
+Enter different train times in the required fields to add them to Firebase and a table to track arrival times with moment.js.
---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCMKhliavttsLO_AaIEO_nqwZWjgW4TY0M",
+    authDomain: "trainscheduler-f4393.firebaseapp.com",
+    databaseURL: "https://trainscheduler-f4393.firebaseio.com",
+    projectId: "trainscheduler-f4393",
+    storageBucket: "",
+    messagingSenderId: "511771441479"
+  };
+  firebase.initializeApp(config);
