4aa3effb6a35c3617631602bdf3474e6aa06069e
DrF <sp.knoll.86@gmail.com>
2018-10-01 15:37:11 -0500

    Train-Time-Firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var config = {
+  var config = {
     apiKey: "AIzaSyCMKhliavttsLO_AaIEO_nqwZWjgW4TY0M",
     authDomain: "trainscheduler-f4393.firebaseapp.com",
     databaseURL: "https://trainscheduler-f4393.firebaseio.com",
@@ -12,42 +12,42 @@ var config = {
 
   var database = firebase.database();
 
-  $("#addTrainBtn").on("click", function() {
+  $("#addTrainBtn").on("click", function () {
     event.preventDefault();
     var trainName = $("#trainNameInput").val().trim();
     var destination = $("#destinationInput").val().trim();
     var firstTrain = $("#trainTimeInput").val().trim();
     var frequency = $("#frequencyInput").val().trim();
-		var firstTime = moment(firstTrain, "hh:mm").subtract(1, "years")
-		var currentTime = moment();
-		var diffTime = moment().diff(moment(firstTime), "minutes");
-		var tRemainder = diffTime % frequency;
-		var minutesTillTrain = frequency - tRemainder;
-		var nextTrain = moment().add(minutesTillTrain, "minutes");
-		var nextTrainTime = moment(nextTrain).format("hh:mm");
+    var firstTime = moment(firstTrain, "hh:mm").subtract(1, "years");
+    var currentTime = moment();
+    var diffTime = moment().diff(moment(firstTime), "minutes");
+    var tRemainder = diffTime % frequency;
+    var minutesTillTrain = frequency - tRemainder;
+    var nextTrain = moment().add(minutesTillTrain, "minutes");
+    var nextTrainTime = moment(nextTrain).format("hh:mm");
     var newRow = [
-    <tr>
-      <td>${trainNameInput}</td>
-      <td>${destinationInput}</td>
-      <td>${frequencyInput}</td>
-      <td>${nextTrainTime}</td>
-      <td>${minutesTillTrain}</td>
-    </tr>
+      <tr>
+        <td>${trainNameInput}</td>
+        <td>${destinationInput}</td>
+        <td>${frequencyInput}</td>
+        <td>${nextTrainTime}</td>
+        <td>${minutesTillTrain}</td>
+      </tr>
     ]
 
     $("#trainSchedule").append(newRow);
-      $("#trainNameInput").val("");
-      $("#destinationInput").val("");
-      $("#trainTimeInput").val("");
-      $("#frequencyInput").val("");
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#trainTimeInput").val("");
+    $("#frequencyInput").val("");
 
-      database.ref().set({
-        trainName: trainName,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: frequency,
-        nextTrainTime: nextTrainTime,
-        minutesTillTrain: minutesTillTrain,
-      })
+    database.ref().set({
+      trainName: trainName,
+      destination: destination,
+      firstTrain: firstTrain,
+      frequency: frequency,
+      nextTrainTime: nextTrainTime,
+      minutesTillTrain: minutesTillTrain,
+    })
   })
 })
